27fc8d7e550b938ce323917dc18244afa619b640
sri <srikumar0908@gmail.com>
2020-06-10 12:21:39 +0530

    oauth

---
+++ src/main/java/com/marlabs/oauth2facebooklogin/Oauth2facebookloginApplication.java
@@ -0,0 +1,13 @@
+package com.marlabs.oauth2facebooklogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2facebookloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2facebookloginApplication.class, args);
+	}
+
+}
